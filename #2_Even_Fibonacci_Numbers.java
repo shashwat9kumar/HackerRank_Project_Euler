9017615ab2161bb323e1888bddb1a579ef810d96
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-04 12:31:39 +0530

    Update #2_Even_Fibonacci_Numbers.java

--- #2_Even_Fibonacci_Numbers.java
+++ #2_Even_Fibonacci_Numbers.java
@@ -14,3 +14,16 @@ public class Solution {
             long a=1;
             long b=2;
             long sum=0;
+            if(b<n)
+            {
+                sum=2;
+                long c=a+b;
+                while (c<n)
+                {
+                    
+                    
+                    a=b;
+                    b=c;
+                    if(c%2==0)
+                    sum=sum+c;
+                    c=a+b;
