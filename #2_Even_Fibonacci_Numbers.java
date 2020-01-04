52555476afc38f2551f7b99915406dcb0dc6d7dd
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-04 12:31:07 +0530

    Update #2_Even_Fibonacci_Numbers.java

--- #2_Even_Fibonacci_Numbers.java
+++ #2_Even_Fibonacci_Numbers.java
@@ -3,3 +3,14 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long t = in.nextLong();
+        for(long a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+            long a=1;
+            long b=2;
+            long sum=0;
