24289d043cf43bde791577047934d0a3b974847b
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-07 20:06:21 +0530

    Update #6_Sum_Square_Difference.cpp

--- #6_Sum_Square_Difference.cpp
+++ #6_Sum_Square_Difference.cpp
@@ -1,6 +1,19 @@
 /*
 
 
+The sum of the squares of the first ten natural numbers is, . The square of the sum of the first ten natural numbers is, . Hence the absolute difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+Find the absolute difference between the sum of the squares of the first  natural numbers and the square of the sum.
+
+Input Format
+
+First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, .
+
+Constraints
+
+Output Format
+
+Print the required answer for each test case.
 Sample Output 0
 
 22
